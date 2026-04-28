09f012388115d665542ca3aefeaaa83f45f9f76f
jhoan ladino <Yhoan Stiven Sanchez>
2026-04-28 10:46:47 -0500

    AC-01 display a list of registered

---
+++ frontend/pages/HU19-ZoneManagement/script.js
@@ -0,0 +1,5 @@
+let zonas = [
+  { id: 1, nombre: "Zona sur",    direccion: "Cll 4 # 13-36", capacidad: 40, cuposLibres: 12, tarifa: 5500,  estado: "activa"    },
+  { id: 2, nombre: "Zona norte",  direccion: "Cll 6 # 13-36", capacidad: 40, cuposLibres: 2,  tarifa: 5000,  estado: "activa"    },
+  { id: 3, nombre: "Zona centro", direccion: "Cll 8 # 13-36", capacidad: 40, cuposLibres: 18, tarifa: 2500,  estado: "cancelada" },
+];
