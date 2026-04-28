27a4c41446269132759da8632569ec3585344f31
David Leon <leondavid1089@gmail.com>
2026-04-27 22:50:27 -0500

    AC-02:The system must include 3 buttons on the right side of the header which will be called: user management, back, exit

---
+++ frontend/pages/HU22-AdminDashboard/script.js
@@ -0,0 +1,15 @@
+
+
+document.getElementById("btnUsuarios").addEventListener("click", () => {
+    window.location.href = '../HU23-UserManagement/index.html'; 
+});
+
+
+document.getElementById("btnAtras").addEventListener("click", () => {
+    window.location.href = '../HU04-HomepageAdmin/index.html';
+});
+
+
+document.getElementById("btnSalir").addEventListener("click", () => {
+    window.location.href = '../HU04-HomepageAdmin/index.html';
+});
