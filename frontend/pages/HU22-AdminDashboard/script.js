5f23b95d7c420d7045fee0f797f2f99d9a751801
David Leon <leondavid1089@gmail.com>
2026-04-27 23:43:26 -0500

    AC-04:The system must include three buttons below the header labeled Today, This Week, and This Month, which display statistics and data related to the requested information.

--- frontend/pages/HU22-AdminDashboard/script.js
+++ frontend/pages/HU22-AdminDashboard/script.js
@@ -13,3 +13,33 @@ document.getElementById("btnAtras").addEventListener("click", () => {
 document.getElementById("btnSalir").addEventListener("click", () => {
     window.location.href = '../HU04-HomepageAdmin/index.html';
 });
+
+const datos = {
+    hoy: {
+        reservas: 189,
+        ingresos: 350000,
+        zonas: 4,
+        usuarios: 120
+    },
+    semana: {
+        reservas: 820,
+        ingresos: 1200000,
+        zonas: 4,
+        usuarios: 300
+    },
+    mes: {
+        reservas: 3200,
+        ingresos: 4800000,
+        zonas: 4,
+        usuarios: 950
+    }
+};
+
+function cargarDatos(periodo) {
+    const info = datos[periodo];
+
+    document.querySelectorAll(".derecha h4")[0].textContent = info.reservas;
+    document.querySelectorAll(".derecha h4")[1].textContent = "$" + info.ingresos;
+    document.querySelectorAll(".derecha h4")[2].textContent = info.zonas;
+    document.querySelectorAll(".derecha h4")[3].textContent = info.usuarios;
+}
