3d1960f9f69590150d0fe7841d57483acb45559d
David Leon <leondavid1089@gmail.com>
2026-04-28 01:21:48 -0500

    I am submitting acceptance criterion number 2

---
+++ frontend/pages/HU23-UserManagement/script.js
@@ -0,0 +1,9 @@
+
+document.getElementById("btnAtras").addEventListener("click", () => {
+    window.location.href = '../HU22-AdminDashboard/index.html';
+});
+
+document.getElementById("btnSalir").addEventListener("click", () => {
+    window.location.href = '../HU04-HomepageAdmin/index.html';
+});
+
