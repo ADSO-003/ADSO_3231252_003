8c7acc9f0973cb96f11e20ab897b3d0676adae18
Sebas Alzate <jmarinalzate64@gmail.com>
2026-05-03 13:15:07 -0500

    feat(HU-08): CA-03 - validaciones de seguridad en cambio de contraseña

--- frontend/pages/HU08-ProfileManagement/script.js
+++ frontend/pages/HU08-ProfileManagement/script.js
@@ -1,2 +1,31 @@
-// HU-08 — Gestión de Perfil de Usuario
-// El código se agrega commit a commit
+function cambiarPassword() {
+
+    let actual = document.getElementById('actual').value;
+    let nueva = document.getElementById('nueva').value;
+    let confirmar = document.getElementById('confirmar').value;
+
+    // Validar campos vacíos
+    if (actual === '' || nueva === '' || confirmar === '') {
+        abrirModal('Campos incompletos',
+            'Debe llenar todos los campos', 'error');
+        return;
+    }
+
+    // Validar que las contraseñas coincidan
+    if (nueva !== confirmar) {
+        abrirModal('Error',
+            'Las contraseñas no coinciden', 'error');
+        return;
+    }
+
+    // Validar longitud mínima (CA-03)
+    if (nueva.length < 6) {
+        abrirModal('Contraseña débil',
+            'La contraseña debe tener mínimo 6 caracteres', 'error');
+        return;
+    }
+
+    abrirModal('Contraseña actualizada',
+        'La contraseña fue cambiada correctamente.');
+}
+
